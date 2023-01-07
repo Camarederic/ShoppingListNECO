3f942c6cd909734aadc2bf456ad039918d20a268
Camarederic <camarederic@mail.ru>
2023-01-07 21:16:15 +0300

    Create a BottomNavigationView

--- app/src/main/java/com/example/shoppinglistneco/activities/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglistneco/activities/MainActivity.kt
@@ -2,11 +2,42 @@ package com.example.shoppinglistneco.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.shoppinglistneco.R
+import com.example.shoppinglistneco.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setBottomNavListener()
+    }
+
+    // метод для нажатия на 4 элемента
+    private fun setBottomNavListener() {
+        binding.bottomNav.setOnItemSelectedListener {
+
+            when (it.itemId) {
+                R.id.settings -> {
+                    Log.d("MyLog", "Settings")
+                }
+                R.id.notes -> {
+                    Log.d("MyLog", "Notes")
+                }
+                R.id.shop_list -> {
+                    Log.d("MyLog", "Shop list")
+                }
+                R.id.new_item -> {
+                    Log.d("MyLog", "New")
+                }
+            }
+
+            true
+        }
     }
 }
