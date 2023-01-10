6fe5580d50ab93afa2fbcd2fa235a2008bfa9c8b
Camarederic <camarederic@mail.ru>
2023-01-10 20:27:42 +0300

    Editing and update notes

--- app/src/main/java/com/example/shoppinglistneco/database/Dao.kt
+++ app/src/main/java/com/example/shoppinglistneco/database/Dao.kt
@@ -5,6 +5,7 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.example.shoppinglistneco.entities.NoteItem
 import kotlinx.coroutines.flow.Flow
 
@@ -19,4 +20,7 @@ interface Dao {
 
     @Query("DELETE FROM note_list WHERE id IS :id")
     suspend fun deleteNote(id: Int)
+
+    @Update
+    suspend fun updateNote(note: NoteItem)
 }
