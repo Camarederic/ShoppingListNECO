18accf726b193d39b7dadf79cff21345df4f5756
Camarederic <camarederic@mail.ru>
2023-02-04 23:06:38 +0300

    Adding two themes for the application style

--- app/src/main/java/com/example/shoppinglistneco/activities/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglistneco/activities/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.shoppinglistneco.activities
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.preference.PreferenceManager
 import com.example.shoppinglistneco.R
 import com.example.shoppinglistneco.databinding.ActivityMainBinding
 import com.example.shoppinglistneco.dialogs.NewListDialog
@@ -15,11 +17,14 @@ import com.example.shoppinglistneco.settings.SettingsActivity
 class MainActivity : AppCompatActivity(), NewListDialog.Listener {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var defPref: SharedPreferences
     private var currentMenuItemId = R.id.shop_list
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        defPref = PreferenceManager.getDefaultSharedPreferences(this)
+        setTheme(getSelectedTheme())
         setContentView(binding.root)
         FragmentManager.setFragment(ShopListNamesFragment.newInstance(), this)
 
@@ -58,6 +63,14 @@ class MainActivity : AppCompatActivity(), NewListDialog.Listener {
     override fun onResume() {
         super.onResume()
         binding.bottomNav.selectedItemId = currentMenuItemId
+    }
+
+    private fun getSelectedTheme(): Int {
+        return if (defPref.getString("theme_key", "blue") == "blue") {
+            R.style.Theme_ShoppingListBlueNECO
+        } else {
+            R.style.Theme_ShoppingListRedNECO
+        }
     }
 
     override fun onClick(name: String) {
